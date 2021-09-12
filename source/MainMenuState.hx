b7ba67fe9ee1b3adbc5fd24ea1f4b348f6516dd1
xalethecat <alexev2401@gmail.com>
2021-09-12 19:06:34 +0300

    mainmenu text fixed

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -103,7 +103,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Grafex Engine v" + modEngineVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Grafex Engine v" + modEngineVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
