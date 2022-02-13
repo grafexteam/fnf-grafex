289bb0ff317f127e1f0a1c944e10505cb972dfaa
PurpleSSSnake <56314743+PurpleSSSnake@users.noreply.github.com>
2022-02-13 21:35:44 +0300

    Version text fix

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var GrafexEngineVersion:String = '0.15'; //This is also used for Discord RPC
+	public static var GrafexEngineVersion:String = '0.1.5'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
