815bc8f215bf920c378d74b68ecfdd46e0931014
PurpleSSSnake <56314743+PurpleSSSnake@users.noreply.github.com>
2022-07-07 21:44:23 +0300

    Alphabet alligment

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -21,7 +21,9 @@ class Alphabet extends FlxSpriteGroup
 
 	// for menu shit
 	public var forceX:Float = Math.NEGATIVE_INFINITY;
+	public var targetX:Float = 0;
 	public var targetY:Float = 0;
+	public var itemType:String = "";
 	public var yMult:Float = 120;
 	public var xAdd:Float = 0;
 	public var yAdd:Float = 0;
@@ -332,19 +334,50 @@ class Alphabet extends FlxSpriteGroup
 		}
 	}
 
-	override function update(elapsed:Float)
+override function update(elapsed:Float)
 	{
 		if (isMenuItem)
 		{
 			var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
 
-			var lerpVal:Float = CoolUtil.boundTo(elapsed * 9.6, 0, 1);
-			y = FlxMath.lerp(y, (scaledY * yMult) + (FlxG.height * 0.48) + yAdd, lerpVal);
-			if(forceX != Math.NEGATIVE_INFINITY) {
-				x = forceX;
-			} else {
-				x = FlxMath.lerp(x, (targetY * 20) + 90 + xAdd, lerpVal);
+			switch (itemType)
+			{
+				case "Classic":
+					y = FlxMath.lerp(y, (scaledY * yMult) + (FlxG.height * 0.48), 0.16/(ClientPrefs.framerate / 60));
+				    x = FlxMath.lerp(x, (targetY * 20) + 90, 0.16/(ClientPrefs.framerate / 60));
+				case "Vertical":
+					y = FlxMath.lerp(y, (scaledY * yMult) + (FlxG.height * 0.5), 0.16/(ClientPrefs.framerate / 60));
+					x = FlxMath.lerp(x, (targetY * 0) + 308, 0.16/(ClientPrefs.framerate / 60));
+					x += targetX/(ClientPrefs.framerate / 60);
+				
+				case "C-Shape":
+					y = FlxMath.lerp(y, (scaledY * 65) + (FlxG.height * 0.39), 0.16/(ClientPrefs.framerate / 60));
+	
+					x = FlxMath.lerp(x, Math.exp(scaledY * 0.8) * 70 + (FlxG.width * 0.1), 0.16/(ClientPrefs.framerate / 60));
+					if (scaledY < 0)
+						x = FlxMath.lerp(x, Math.exp(scaledY * -0.8) * 70 + (FlxG.width * 0.1), 0.16/(ClientPrefs.framerate / 60));
+	
+					if (x > FlxG.width + 30)
+						x = FlxG.width + 30;
+				case "D-Shape":
+					y = FlxMath.lerp(y, (scaledY * 90) + (FlxG.height * 0.45), 0.16/(ClientPrefs.framerate / 60));
+		
+					x = FlxMath.lerp(x, Math.exp(scaledY * 0.8) * -70 + (FlxG.width * 0.35), 0.16/(ClientPrefs.framerate / 60));
+					if (scaledY < 0)
+						x = FlxMath.lerp(x, Math.exp(scaledY * -0.8) * -70 + (FlxG.width * 0.35), 0.16/(ClientPrefs.framerate / 60));
+		
+					if (x < -900)
+						x = -900;
+				default:
+					var lerpVal:Float = CoolUtil.boundTo(elapsed * 9.6, 0, 1);
+			        y = FlxMath.lerp(y, (scaledY * yMult) + (FlxG.height * 0.48) + yAdd, lerpVal);
+			        if(forceX != Math.NEGATIVE_INFINITY) {
+			        	x = forceX;
+			        } else {
+			        	x = FlxMath.lerp(x, (targetY * 20) + 90 + xAdd, lerpVal);
+			        }
 			}
+
 		}
 
 		super.update(elapsed);
