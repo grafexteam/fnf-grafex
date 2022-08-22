0c9cb7f7c622fc47b7306b9253dda11dff6aba77
xalethecat <alexev2401@gmail.com>
2022-08-22 11:56:43 +0300

    tests done

--- source/grafex/states/playstate/PlayState.hx
+++ source/grafex/states/playstate/PlayState.hx
@@ -392,7 +392,6 @@ class PlayState extends MusicBeatState
 	override public function create()
 	{
 		Paths.clearStoredMemory();
-		throw new haxe.Exception('TEST LOL :))))))))');
 
 		instance = this;
         //currentPState = this;
