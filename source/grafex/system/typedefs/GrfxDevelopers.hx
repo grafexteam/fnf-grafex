3c4bc9c83060e2b5e24a9eae13085e810e3fbb34
xalethecat <alexev2401@gmail.com>
2022-08-22 17:56:22 +0300

    will done later

---
+++ source/grafex/system/typedefs/GrfxDevelopers.hx
@@ -0,0 +1,9 @@
+package grafex.system.typedefs;
+
+typedef GrfxDevelopers = {   
+    var nick:String;
+    @:optional var icon:String;
+    @:optional var role:String;
+    @:optional var socialLink:String;
+    @:optional var bgColor:String; 
+}
